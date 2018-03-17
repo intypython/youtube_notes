cb5a5ba10cd6359820d4f58ef4ec8766b2c4997d
intypython <intypython@gmail.com>
2018-03-16 22:10:49 -0500

    tuple练习的源代码 Source code for video 7

---
+++ tuplesPractise.py
@@ -0,0 +1,33 @@
+# list
+
+mylist = [1, 2, 3, 4, 5]
+
+# tuple
+mytuple = (1, 2, 3, 4, 5)
+
+# print(type(mylist))
+#
+# print(type(mytuple))
+#
+# print(len(mylist))
+# print(len(mytuple))
+#
+# print(mytuple[0])
+# print(mylist[0])
+
+#
+# print(dir(mylist))
+#
+# print("..........................")
+#
+# print(dir(mytuple))
+
+
+# list is mutable
+# tuple is inmutable
+#
+# mylist.remove(2)
+# print(mylist)
+
+mytuple.remove(2)
+print(mytuple)
