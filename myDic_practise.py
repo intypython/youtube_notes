ec47ab177a421bfa3de38b69df3717deead3de56
intypython <intypython@gmail.com>
2018-03-23 10:33:34 -0500

    dictionary

---
+++ myDic_practise.py
@@ -0,0 +1,19 @@
+
+# myList={1,2,3,4}
+# myDictionary={"key":"value","key2":"value"}
+#
+#
+myPhones={"Iphone X":1000,"Sumsang S9":900}
+#
+# print(myPhones)
+# # access dic keys
+#
+# Iphpne_Price=myPhones["Iphone X"]
+# print("IPhone price: "+str(Iphpne_Price))
+#
+# # change key values
+# myPhones["Iphone X"]=500
+# print(myPhones)
+
+myPhones.clear()
+print(myPhones)
