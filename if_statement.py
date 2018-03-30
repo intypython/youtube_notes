071e7db62c887f07cf397d62fe1589539af0e186
intypython <intypython@gmail.com>
2018-03-30 11:00:51 -0500

    if elif else statement

--- if_statement.py
+++ if_statement.py
@@ -1,11 +1,26 @@
 
 
 
+#
+# a=10
+# b=20
+#
+# if b>a:
+#     print("hello")
+# if b<30:
+#     print("another hello")
 
-a=10
-b=20
 
-if b>a:
-    print("hello")
-if b<30:
-    print("another hello")
+age=int(input("please enter your age: "))
+
+if age<21:
+    print("you can not smoke")
+
+elif age==21:
+    print("you are now 21,you can go to smoke")
+
+elif age==100:
+    print("you are 100 years old,please quit smoking")
+
+else:
+    print("you can smoke")
