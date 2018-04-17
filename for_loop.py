0b2b41dc5d2cafa3f93d049a1e7d1be822ff3510
intypython <intypython@gmail.com>
2018-04-17 00:10:46 -0500

    update

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+
+
+
+my_list=["jake","Tom","xi li"]
+
+
+
+# for name in my_list:
+#     print(name)
+
+# for number in range(11):
+#     print(number)
+
+
+for letter in 'inty':
+    print("each letter is: "+letter)
---
+++ func_practise.py
@@ -0,0 +1,38 @@
+# # define 下定义
+# def func1():
+#     print("hello function")
+#
+#
+#
+# func1()
+
+#
+# a = 10
+# b = 10
+#
+# print(a + b)
+#
+# c = 20
+# d = 20
+# print(c + d)
+
+
+# def sum(num1,num2):
+#     print(num1+num2)
+#
+# sum(1,2)
+# sum(10,10)
+
+
+
+
+def fun2():
+    return "hello fun2"
+
+def sum2(a,b):
+    return (a+b)
+
+
+b=sum2(1,1)
+
+print(b)
