7930a15a22859e0428efb19da99c939f26d2caeb
inty <intypython@gmail.com>
2018-09-09 00:19:29 -0500

    selenium 教学，怎么截图 09/09/2018

---
+++ seleniumDemo/takeScreenShot.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('/Users/inty/Downloads/chromedriver')
+driver.fullscreen_window()
+driver.get('https://www.baidu.com/')
+driver.find_element_by_id('kw').send_keys('this is Inty,youtube demo 大家好')
+
+driver.save_screenshot('/Users/inty/desktop/截图.png')
+
+driver.close()
+
+
+
